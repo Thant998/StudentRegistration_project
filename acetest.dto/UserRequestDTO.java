79dfb453a0d7de1c26e5cc980799e620247d537e
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-08-15 13:29:03 +0630

    Add files via upload

---
+++ acetest.dto/UserRequestDTO.java
@@ -0,0 +1,39 @@
+package acetest.dto;
+
+public class UserRequestDTO {
+	private String userEmail;
+	private String userPassword;
+	private int userRole;
+	private String userId;
+	private String userName;
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public int getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(int userRole) {
+		this.userRole = userRole;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
