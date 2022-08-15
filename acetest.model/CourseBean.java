70a7d8a84a17cbc97ca3663b6310e0a5fa4ca333
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-08-15 13:31:43 +0630

    Add files via upload

---
+++ acetest.model/CourseBean.java
@@ -0,0 +1,18 @@
+package acetest.model;
+
+public class CourseBean {
+	private String id;
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
