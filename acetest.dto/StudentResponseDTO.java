b64d54fa45bab32091038ae7b8b2c756077769f3
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-08-15 13:28:45 +0630

    Add files via upload

---
+++ acetest.dto/StudentResponseDTO.java
@@ -0,0 +1,54 @@
+package acetest.dto;
+
+public class StudentResponseDTO {
+	private String studentId;
+	private String studentName;
+	private String studentBirthday;
+	private String studentGender;
+	private String studentPhone;
+	private String studentEducation;
+	private int studentAttend;
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentBirthday() {
+		return studentBirthday;
+	}
+	public void setStudentBirthday(String studentBirthday) {
+		this.studentBirthday = studentBirthday;
+	}
+	public String getStudentGender() {
+		return studentGender;
+	}
+	public void setStudentGender(String studentGender) {
+		this.studentGender = studentGender;
+	}
+	public String getStudentPhone() {
+		return studentPhone;
+	}
+	public void setStudentPhone(String studentPhone) {
+		this.studentPhone = studentPhone;
+	}
+	public String getStudentEducation() {
+		return studentEducation;
+	}
+	public void setStudentEducation(String studentEducation) {
+		this.studentEducation = studentEducation;
+	}
+	public int getStudentAttend() {
+		return studentAttend;
+	}
+	public void setStudentAttend(int studentAttend) {
+		this.studentAttend = studentAttend;
+	}
+	
+}
