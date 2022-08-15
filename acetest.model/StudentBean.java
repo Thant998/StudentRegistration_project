96d9b0ebb25f43ff6d0c8cf089058f96d0560202
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-08-15 13:30:59 +0630

    Add files via upload

---
+++ acetest.model/StudentBean.java
@@ -0,0 +1,53 @@
+package acetest.model;
+
+public class StudentBean {
+	private String id;
+	private String name;
+	private String birthday;
+	private String gender;
+	private String phone;
+	private String education;
+	private int course;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public int getCourse() {
+		return course;
+	}
+	public void setCourse(int course) {
+		this.course = course;
+	}
+}
