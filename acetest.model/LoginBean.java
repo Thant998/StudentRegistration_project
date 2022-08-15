5daa0cf93009a69a52e67c4aeceb7d7214880269
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-08-15 13:31:27 +0630

    Add files via upload

---
+++ acetest.model/LoginBean.java
@@ -0,0 +1,18 @@
+package acetest.model;
+
+public class LoginBean {
+	private String loginName;
+	private String loginPassword;
+	public String getLoginName() {
+		return loginName;
+	}
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+}
